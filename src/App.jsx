e0ba3fe76a16868b207805dfa0991a92c59f1d47
MateoR-gif <mateorendon008@gmail.com>
2023-05-16 20:49:57 -0500

    Dark Mode Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import "./App.css";
+import "./css/App.css";
 import Loading from "./pages/loading/loading";
 import Home from "./pages/home/home";
 import Footer from "./pages/home/components/footer";
+import ToggleDark from "./ToggleDark";
 
 function App() {
   return (
     <div className="app">
+      <header>
+        <ToggleDark />
+      </header>
       <main className="content">
         <Loading>
           <Home path="/" />
--- src/pages/loading/loading.jsx
+++ src/pages/loading/loading.jsx
@@ -1,19 +1,19 @@
-import loadingGif from "../../assets/loadingPage/gifs/loadingGif.gif";
+/* import loadingGif from "../../assets/loadingPage/gifs/loadingGif.gif"; */
 import "./css/loadingPage.css";
 import useLoading from "../../hooks/useLoading";
 
 export default function Loading({ children }) {
-  const { msg, loading, switchAsset } = useLoading();
+  const { msg, loading } = useLoading();
 
   if (loading) {
     return (
       <div className="loadingPage">
         <p>{msg}</p>
-        {switchAsset ? (
+        {/* {switchAsset ? (
           <img src={loadingGif} alt="loadinggif"></img>
         ) : (
           <img src={loadingGif} alt="loadinggif"></img>
-        )}
+        )} */}
       </div>
     );
   }
