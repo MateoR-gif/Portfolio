b0c1715f408fd3e15594faa3854a76afa57c1354
MateoR-gif <mateorendon008@gmail.com>
2023-05-18 14:21:31 -0500

    Embedded PDF Update

--- src/pages/home/components/folderStructure.jsx
+++ src/pages/home/components/folderStructure.jsx
@@ -1,6 +1,6 @@
 export const folderStructure = {
     name: "Mateo's Directory",
-    files: [{name: 'CV', type: 'CV', url: 'https://drive.google.com/file/d/14B0lbWCsl4p-w2VN938hhSi8PC36Ciex/view?usp=sharing/home/NCV.pdf'}],
+    files: [{name: 'CV', type: 'CV', url: 'https://drive.google.com/file/d/14B0lbWCsl4p-w2VN938hhSi8PC36Ciex/view?usp=sharing'}],
     subfolders: [
         {
             name: 'Projects',
