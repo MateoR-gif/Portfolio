6f9182f642035093939f7f39edaa4b3f148784b6
MateoR-gif <mateorendon008@gmail.com>
2023-05-18 18:02:29 -0500

    Minor Style Changes

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -18,6 +18,7 @@ export default function Home() {
     return (
       <article>
         <Content info={contentInfo} />
+        <br />
         <p className='goback link' onClick={() => setIsContentShow(false)}>{'Go back'}</p>
       </article>
     )
