5b3be5d6de7a69055b43b0f3ec323c720a7976e1
MateoR-gif <mateorendon008@gmail.com>
2023-05-18 15:00:06 -0500

    Embedded PDF Update

--- src/pages/home/components/PDFViewer.jsx
+++ src/pages/home/components/PDFViewer.jsx
@@ -4,7 +4,7 @@ export default function PDFViewer({ info }) {
     <div className="PDFViewer">
       <h1>Curriculum Vitae</h1>
       <div style={{ width: '100%', height: '90%' }}>
-        <iframe src="https://drive.google.com/file/d/14B0lbWCsl4p-w2VN938hhSi8PC36Ciex/preview" width="640" height="480" allow="autoplay"></iframe>F
+        <iframe src={url} title={name} style={{ width: '100%', height: '100%' }} allow="autoplay"/>
       </div>
     </div>
   )
--- src/pages/home/components/folderStructure.jsx
+++ src/pages/home/components/folderStructure.jsx
@@ -1,20 +1,20 @@
 export const folderStructure = {
     name: "Mateo's Directory",
-    files: [{name: 'CV', type: 'CV', url: 'https://drive.google.com/file/d/14B0lbWCsl4p-w2VN938hhSi8PC36Ciex/view?usp=sharing'}],
+    files: [{ name: 'CV', type: 'CV', url: "https://drive.google.com/file/d/14B0lbWCsl4p-w2VN938hhSi8PC36Ciex/preview" }],
     subfolders: [
         {
             name: 'Projects',
-            files: [{name: 'on build'}],
+            files: [{ name: 'on build' }],
             subfolders: [],
         },
         {
             name: 'Certifications',
-            files: [{name: 'on build'}],
+            files: [{ name: 'on build' }],
             subfolders: [],
         },
         {
             name: 'Laboral Experience',
-            files: [{name: 'on build'}],
+            files: [{ name: 'on build' }],
             subfolders: []
         },
     ],
