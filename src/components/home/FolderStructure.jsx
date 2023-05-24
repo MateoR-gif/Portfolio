65886e11dcdacbb868e1f0578a45b9355dee48bf
MateoR-gif <mateorendon008@gmail.com>
2023-05-23 20:40:12 -0500

    Minor Styles Changes

--- src/components/home/FolderStructure.jsx
+++ src/components/home/FolderStructure.jsx
@@ -1,101 +1,143 @@
 export const folderStructure = {
-    name: "Mateo's Directory",
-    files: [{
-        name: 'CV',
-        type: 'PDF',
-        largeName: 'Curriculum Vitae',
-        url: "https://drive.google.com/file/d/14B0lbWCsl4p-w2VN938hhSi8PC36Ciex/preview",
-        description: ""
-    }],
-    subfolders: [
+  name: "Mateo's Directory",
+  files: [
+    {
+      name: "CV",
+      type: "PDF",
+      largeName: "Curriculum Vitae",
+      url: "https://drive.google.com/file/d/14B0lbWCsl4p-w2VN938hhSi8PC36Ciex/preview",
+      description: "",
+    },
+  ],
+  subfolders: [
+    {
+      name: "Projects",
+      files: [
         {
-            name: 'Projects',
-            files: [{
-                name: 'Crapp',
-                type: 'Project',
-                largeName: 'Crapp',
-                url: "",
-                description: "This project consists of a garbage collection ticket generator in sectors where its accumulation is critical, so that collectors prioritize these sectors. Implemented Technologies: React - Firebase - Java Springboot"
-            },
-            {
-                name: "LT",
-                type: 'Project',
-                largeName: "Let's Talk",
-                url: "https://github.com/MateoR-gif/LT-Front",
-                description: "This is an easy-to-use real-time chat. Implemented Technologies: React, MongoDB, ExpressJS, Web Sockets."
-            },
-            {
-                name: "MS",
-                type: "Project",
-                largeName: "Movie Searcher",
-                url: "https://github.com/MateoR-gif/MovieSearcher-React",
-                description: "This is a movie sarcher, using a public API. Implemented Technologies: ReactJS."
-            }],
-            subfolders: [],
+          name: "Crapp",
+          type: "Project",
+          largeName: "Crapp",
+          url: "",
+          description:
+            "This project consists of a garbage collection ticket generator in sectors where its accumulation is critical, so that collectors prioritize these sectors. Implemented Technologies: React - Firebase - Java Springboot",
         },
         {
-            name: 'Certifications',
-            files: [],
-            subfolders: [{
-                name: 'SAP',
-                files: [{
-                    name: 'ABAP Basics',
-                    type: 'PDF',
-                    largeName: 'SAP Iniciación a la Programación',
-                    url: "https://drive.google.com/file/d/1dvyS4t_xdGFvjFijARdbiCD2Q7ht8iAQ/preview",
-                    description: ""
-                }],
-                subfolders: []
+          name: "LT",
+          type: "Project",
+          largeName: "Let's Talk",
+          url: "https://github.com/MateoR-gif/LT-Front",
+          description:
+            "This is an easy-to-use real-time chat. Implemented Technologies: React, MongoDB, ExpressJS, Web Sockets.",
+        },
+        {
+          name: "MS",
+          type: "Project",
+          largeName: "Movie Searcher",
+          url: "https://github.com/MateoR-gif/MovieSearcher-React",
+          description:
+            "This is a movie sarcher, using a public API. Implemented Technologies: ReactJS.",
+        },
+      ],
+      subfolders: [],
+    },
+    {
+      name: "Certifications",
+      files: [],
+      subfolders: [
+        {
+          name: "SAP",
+          files: [
+            {
+              name: "ABAP Basics",
+              type: "PDF",
+              largeName: "SAP Iniciación a la Programación",
+              url: "https://drive.google.com/file/d/1dvyS4t_xdGFvjFijARdbiCD2Q7ht8iAQ/preview",
+              description: "",
+            },
+          ],
+          subfolders: [],
+        },
+        {
+          name: "Python",
+          files: [
+            {
+              name: "Python Foundamentals",
+              type: "PDF",
+              largeName: "Fundamentos de Python",
+              url: "https://drive.google.com/file/d/19lKziF6GtZsOM9ZBYcZu9inIfnjSc6s1/preview",
+              description: "",
             },
             {
-                name: 'Python',
-                files: [{
-                    name: 'Python Foundamentals',
-                    type: 'PDF',
-                    largeName: 'Fundamentos de Python',
-                    url: "https://drive.google.com/file/d/19lKziF6GtZsOM9ZBYcZu9inIfnjSc6s1/preview",
-                    description: ""
-                },
-                {
-                    name: 'Comprehension, Functions, Error Handling',
-                    type: 'PDF',
-                    largeName: 'Python: Comprehensions, funciones y manejo de errores.',
-                    url: "https://drive.google.com/file/d/1_M6Juugw4XIdbWY_8NvF9DZMUNGjQoKf/preview",
-                    description: ""
-                }],
-                subfolders: []
+              name: "Comprehension, Functions, Error Handling",
+              type: "PDF",
+              largeName:
+                "Python: Comprehensions, funciones y manejo de errores.",
+              url: "https://drive.google.com/file/d/1_M6Juugw4XIdbWY_8NvF9DZMUNGjQoKf/preview",
+              description: "",
+            },
+          ],
+          subfolders: [],
+        },
+        {
+          name: "Java",
+          files: [
+            {
+              name: "JavaSE Introduction",
+              type: "PDF",
+              largeName: "Introducción a Java SE",
+              url: "https://drive.google.com/file/d/1Fqe_qBrZkjtLOM7SeCBHXZKy8Qeq4YPa/preview",
             },
             {
-                name: 'Java',
-                files: [{
-                    name: 'JavaSE Introduction',
-                    type: 'PDF',
-                    largeName: 'Introducción a Java SE',
-                    url: "https://drive.google.com/file/d/1Fqe_qBrZkjtLOM7SeCBHXZKy8Qeq4YPa/preview"
-                },
+              name: "JavaSE OOP",
+              type: "PDF",
+              largeName: "Java SE Orientado a Objetos",
+              url: "https://drive.google.com/file/d/1T4WPcCB_DE-k_viukXkKhj-njd6IFuC6/preview",
+            },
+          ],
+          subfolders: [],
+        },
+        {
+          name: "Design Patterns",
+          files: [
+            {
+              name: "Design Patterns Introduction",
+              type: "PDF",
+              largeName: "Introducción a los Patrones de Diseño de Software",
+              url: "https://drive.google.com/file/d/1t3OgkZituf2dOJrIwfGlvdcOrxy9kd3E/preview",
+            },
+          ],
+          subfolders: [],
+        },
+        {
+            name: "React",
+            files: [
                 {
-                    name: "JavaSE OOP",
+                    name: "ReactJS Practice's Course",
                     type: "PDF",
-                    largeName: "Java SE Orientado a Objetos",
-                    url: "https://drive.google.com/file/d/1T4WPcCB_DE-k_viukXkKhj-njd6IFuC6/preview"
-                }],
-                subfolders: []
-            },
-            {
-                name: 'Design Patterns',
-                files: [{
-                    name: 'Design Patterns Introduction',
-                    type: 'PDF',
-                    largeName: 'Introducción a los Patrones de Diseño de Software',
-                    url: 'https://drive.google.com/file/d/1t3OgkZituf2dOJrIwfGlvdcOrxy9kd3E/preview'
-                }],
-                subfolders: []
-            }],
-        },
-        {
-            name: 'Laboral Experience',
-            files: [{ name: 'on build' }],
+                    largeName: "Curso Práctico de ReactJS",
+                    url: "https://drive.google.com/file/d/19L5dwmf_3469A1Fs947s06MT30WZ_TBc/preview"
+                }
+            ],
             subfolders: []
         },
-    ],
+        {
+            name: "Git",
+            files: [
+                {
+                    name: "Git & Github",
+                    type: "PDF",
+                    largeName: "Curso Profesional de Git y Github",
+                    url: "https://drive.google.com/file/d/16OsupPvBXeLCvCgcH-wDGHHmsNfOqJMQ/preview"
+                }
+            ],
+            subfolders: []
+        }
+      ],
+    },
+    {
+      name: "Laboral Experience",
+      files: [{ name: "on build" }],
+      subfolders: [],
+    },
+  ],
 };
